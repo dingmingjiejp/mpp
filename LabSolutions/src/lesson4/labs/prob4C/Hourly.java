bbaeba080d53856f8997b537291ef3c4b81655c5
JiLiang <dindin0497@sina.com>
2017-09-08 09:59:58 -0500

    4

--- LabSolutions/src/lesson4/labs/prob4C/Hourly.java
+++ LabSolutions/src/lesson4/labs/prob4C/Hourly.java
@@ -13,7 +13,7 @@ public class Hourly extends Employee{
 	}
 	@Override
 	public double calcGrossPay(int month, int year) {
-		return hoursPerWeek*hourlyWage;
+		return hoursPerWeek*hourlyWage*4;
 	}
 
 }
