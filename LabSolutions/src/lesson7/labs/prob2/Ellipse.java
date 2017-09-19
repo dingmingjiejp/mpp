7f27762f35a600d2676a9b2fae285abbf96f777c
mingjieding <dingmingjiejp@gmail.com>
2017-09-18 20:11:54 -0500

    prob 2 commit

--- LabSolutions/src/lesson7/labs/prob2/Ellipse.java
+++ LabSolutions/src/lesson7/labs/prob2/Ellipse.java
@@ -2,23 +2,23 @@ package lesson7.labs.prob2;
 
 public class Ellipse implements ClosedCurve {
     private double semiMajorAxis;
-    private double semiMinorAxis;
+    private double eccentricity;
 
-    public Ellipse(double semiMajorAxis, double semiMinorAxis) {
+    public Ellipse(double semiMajorAxis, double eccentricity) {
         this.semiMajorAxis = semiMajorAxis;
-        this.semiMinorAxis = semiMajorAxis;
+        this.eccentricity = eccentricity;
     }
 
     public double getSemiMajorAxis() {
         return semiMajorAxis;
     }
 
-    public double getSemiMinorAxis() {
-        return semiMinorAxis;
+    public double getEccentricity() {
+        return eccentricity;
     }
 
     @Override
     public double computePerimeter() {
-        return 4 * this.semiMinorAxis * this.semiMajorAxis;
+        return 4 * this.eccentricity * this.semiMajorAxis;
     }
 }
