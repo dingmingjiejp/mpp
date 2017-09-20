40131e17b9a581b455f65cd82ada104416d04eab
jargalan <jargalan@172.18.2.240>
2017-09-19 21:27:21 -0500

    lab8 prob2

--- LabSolutions/src/lesson8/labs/prob2/comparator2/EmployeeNameComparator.java
+++ LabSolutions/src/lesson8/labs/prob2/comparator2/EmployeeNameComparator.java
@@ -8,9 +8,7 @@ public class EmployeeNameComparator implements Comparator<Employee> {
 	public int compare(Employee e1, Employee e2) {
 		if(e1.name.compareTo(e2.name) == 0) {
 			if(e1.equals(e2)) return 0;
-			else if(e1.salary == e2.salary) return 0;
-			else if(e1.salary < e2.salary) return -1;
-			else return 1;
+			else return -1;
 		}
 		return e1.name.compareTo(e2.name);
 	}
