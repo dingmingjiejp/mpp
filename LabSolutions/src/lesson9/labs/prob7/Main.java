c32838a01cfd7be52314a187bbdf3d6bb690bb75
jargalan <jargalanch@gmail.com>
2017-09-21 17:21:24 -0500

    lab9 part2 prob7

--- LabSolutions/src/lesson9/labs/prob7/Main.java
+++ LabSolutions/src/lesson9/labs/prob7/Main.java
@@ -13,29 +13,37 @@ public class Main {
 		List<String> stringList = Arrays.asList("cba", "efg", "doe", "fie", "set");
 		//expected output: [cba, fie, doe, efg, set]
 		ordering2(stringList);
-		
+
 	}
-	
+
 	//Orders the integers according to this pattern:
 	// 0, -1, 1, -2, 2, -3, 3, . . .
-	//Using this ordering, this method sorts the list as part of 
+	//Using this ordering, this method sorts the list as part of
 	//a stream pipeline, and prints to the console
 	public static void ordering1(List<Integer> list) {
-		System.out.println(/* implement */);
-		
+		Function<Integer, Integer> byBigger = n -> Math.abs(n);
+		Function<Integer, Integer> bySign = n -> (n > 0 ? 1 : -1);
+
+		System.out.println(list.stream().sorted(Comparator
+				.comparing(byBigger)
+				.thenComparing(bySign)).collect(Collectors.toList()));
+
 	}
-	
-	//Orders words by first reversing each and comparing 
-	//in the usual way.  So 
+
+	//Orders words by first reversing each and comparing
+	//in the usual way.  So
 	//    cba precedes bed
 	//since
 	//    cba.reverse() precedes bed.reverse()
 	//in the usual ordering
-	//Using this ordering, this method sorts the list as part of 
+	//Using this ordering, this method sorts the list as part of
 	//a stream pipeline, and prints to the console
 	public static void ordering2(List<String> words) {
-		System.out.println(/* implement */);
-				
+		Function<String, String> byReverse = n -> (new StringBuilder(n).reverse().toString());
+
+		System.out.println(words.stream().sorted(Comparator
+				.comparing(byReverse)).collect(Collectors.toList()));
+
 	}
 
 }
