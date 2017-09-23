5e73b32417179f50b49ecf9284f18b225fb5be04
jargalan <jargalanch@gmail.com>
2017-09-23 17:53:25 -0500

    prob 2

--- LabSolutions/src/lesson10/labs/prob2/partA/OldFileIO.java
+++ LabSolutions/src/lesson10/labs/prob2/partA/OldFileIO.java
@@ -1,4 +1,4 @@
-package lesson10.labs.prob2;
+package lesson10.labs.prob2.partA;
 
 import java.io.*;
 import java.io.File;
@@ -11,9 +11,9 @@ import java.util.logging.Logger;
 
 public class OldFileIO {
 	private static final Logger LOG = Logger.getLogger(OldFileIO.class.getName());
-	public final static String FILE_LOCATION = "src/lesson10/labs/prob3/output.txt";
+	public final static String FILE_LOCATION = "src/lesson10/labs/prob2/output.txt";
 	public final static String TO_PRINT = "This is the string to print to file.";
-	
+
 	void writeText(String filename, String text) {
 		FileWriter fw = null;
 		PrintWriter writer = null;
@@ -24,20 +24,22 @@ public class OldFileIO {
 		writer.print(text);
 		} catch(IOException e) {
 			LOG.warning("IOException attempting to write a file: " + e.getMessage());
+			System.out.println(e);
 		} finally {
 			try {
 				if(fw != null) fw.close();
 				if(writer != null) writer.close();
 			} catch(IOException ex) {
 				LOG.warning("IOException attempting to close Writers: " + ex.getMessage());
+				System.out.println(ex);
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		OldFileIO old = new OldFileIO();
 	    old.writeText(FILE_LOCATION, TO_PRINT);
-		
+
 	}
 
 }
